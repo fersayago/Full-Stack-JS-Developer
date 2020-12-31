ed6d790f2c4e9541e169d4f232c65faa02d26afc
fersayago <sayago.fernando@gmail.com>
2020-12-31 03:18:06 -0300

    recuperando curso JS Avanzado

---
+++ Javascript avanzado/Schwab/clase1/JS/script.js
@@ -0,0 +1,30 @@
+var data = {
+  dia: "Lunes", // propiedad (clave : valor) separadas por coma
+  clima: "nublado",
+  temperatura: 18.6,
+  actualizar: function(){ //metodo (porque su valor es una fn)
+    console.log("actualizando...");
+  }
+};
+
+// NOTACION DE PUNTO (Preferido)
+// leer un valor
+console.log( data.temperatura ); // 18.6
+// modificar un valor
+data.temperatura = 20.3;
+
+// NOTACION DE CORCHETES
+console.log( data["clima"]); // data.clima = "nublado"
+data["clima"] = "soleado"
+
+
+// caso real de uso de notación de corchetes
+var entrada = "dia";
+
+function buscar (propiedadAMostrar){
+  console.log( data.propiedadAMostrar); // undefined
+  console.log( data[propiedadAMostrar]); // si se invoca dia = "lunes"
+}
+
+//buscamos dia en data con la funcion
+buscar("dia");
