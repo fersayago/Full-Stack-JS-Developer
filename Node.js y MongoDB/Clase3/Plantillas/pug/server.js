d04191c1b073ff61504099a99f7a4aae4728acb5
fersayago <sayago.fernando@gmail.com>
2021-01-14 22:51:18 -0300

    2021-01-14 Node.js y MongoDB - Clase 3 (CRUD Express postman, Modulos y Plantillas)

---
+++ Node.js y MongoDB/Clase3/Plantillas/pug/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pug = require('pug');
+
+const app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    title: 'Página Pug', 
+    message: 'Hola Juan',
+    usuarios: [
+      'Pedro',
+      'María',
+      'Pepe'
+    ]
+  });
+});
+
+const PORT = 8080
+app.listen(PORT, function(){
+	console.log('Server up in port ' + PORT);
+});
---
+++ Node.js y MongoDB/Clase3/modulos/import/api/operaciones.js
@@ -0,0 +1,23 @@
+export const suma = (a,b) => a + b
+export const resta = (a,b) => a - b
+export const mult = (a,b) => a * b
+export const div = (a,b) => a / b
+export const mod = (a,b) => a % b
+
+//export ES Modules
+
+/* export default {
+  suma,
+  resta,
+  mult,
+  div,
+  mod
+} */
+
+/* export {
+  suma,
+  resta,
+  mult,
+  div,
+  mod
+} */
