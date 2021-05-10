c3f762b2ce3abf1d6187a97a110e161d7c3273c3
fersayago <sayago.fernando@gmail.com>
2021-05-10 18:05:36 -0300

    recomenzado curso

--- React.js/clase1/1.ejemploBabel/index.ES6.js
+++
@@ -1,8 +0,0 @@
-let mensaje = 'Hola Mundo!'
-console.log(mensaje)
-
-const sumar = (a, b) => a + b
-const dobleDe = a => 2*a
-
-console.log(`La suma de ${sumar(14,3)}`)
-console.log(`El doble de 8 es ${dobleDe(8)}`)
