55f85cd7c6a29e1a1a5362d498c26c2f8cc7faeb
fersayago <sayago.fernando@gmail.com>
2021-05-10 23:34:01 -0300

    primera mitad clase 1: babel y webpack

---
+++ React.js/clase01/ejemplo-webpack/index.WP.js
@@ -0,0 +1,11 @@
+console.log('-------------------------index.WP.js-------------------------')
+
+function dobleDe(a) {
+  return 2*a
+}
+
+var num = 30
+
+console.log('El cuadruple de' + n + ' es ' + dobleDe(dobleDe(n)))
+
+console.log('-------------------------index.WP.js-------------------------')
