2c652859dc859ec4a4f788083a16bed7d85f0777
fersayago <sayago.fernando@gmail.com>
2021-05-17 23:20:15 -0300

    2021-05-17: react.JS: Babel, Webpack y create-react-App

---
+++ React.js/clase01/mi-primer-proyecto/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mt-3">
+        <div className="jumbotron">
+          <h1>Mi proyecto React</h1>
+          <hr />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab facere, sed magni a dolorum excepturi vitae dolore id! Tempora molestias perferendis accusamus facere assumenda nemo necessitatibus neque fuga aut iure.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
