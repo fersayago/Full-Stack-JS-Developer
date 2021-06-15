c4db585cfacd8f34d04fe6d5e7bfa073300cdc81
Fernando Sayago <sayago.fernando@gmail.com>
2021-06-14 23:17:15 -0300

    2021-06-14 JS Avanzado - Clase 5: Ajax, SPA y router

---
+++ Javascript avanzado/clase05/JS/router.js
@@ -0,0 +1,14 @@
+const routes = {
+  "/home": {
+    url: "views/home/index.html",
+    titulo: "Bienvenidos!"
+  },
+  "/products": {
+    url: "views/productos.html",
+    titulo: "Nuestros productos"
+  },
+  "/contact": {
+    url: "views/contacto.html",
+    titulo: "Estamos para usted" 
+  }
+}
