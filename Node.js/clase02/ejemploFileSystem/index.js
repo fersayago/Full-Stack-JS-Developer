473a7edd799975009cce3411bf0addc0fa73fd9e
fersayago <sayago.fernando@gmail.com>
2021-08-09 18:12:22 -0300

    lectura y escritura de archivos sincronico y asincronico

--- Node.js/clase02/ejemploFileSystem/index.js
+++ Node.js/clase02/ejemploFileSystem/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // ? DOCUMENTACIÓN FILESYSTEM:
 // ? https://nodejs.org/dist/latest-v16.x/docs/api/fs.html
@@ -9,4 +9,58 @@ const fs = require('fs');
 
 // no conviene usar esta forma para ciertos procesos (ej: lectura y escritura de archivos) ya que bloquea el servidor de node.js para otros procesos.
 
-// ! retomar 56:30
+/*
+console.log('Inicio del programa')
+
+ try {
+  // LEO UN ARCHIVO DE FORMA SINCRONICA
+  
+  // para que no lea en formato de buffer (hex) es necesario pasarle el formato utf-8 para poder verlo en formato de string.
+  let datos = fs.readFileSync('./datos.txt', 'utf-8');
+  console.log('RD1 ok', datos, datos.length);
+
+  //ESCRIBO UN ARCHIVO DE FORMA SINCRONICA
+
+  fs.writeFileSync('./datos.txt', new Date().toLocaleString())
+  console.log('WR ok')
+
+  datos = fs.readFileSync('./datos.txt', 'utf-8');
+  console.log('RD2 ok', datos, datos.length);
+}
+catch (error) {
+  console.log(`Hubo un error en la operacion sincronica fs del tipo: ${error}`);
+}
+
+console.log('... otras tareas ...') */
+
+/* --------------------------------------------------- */
+/*      READ / WRITE FILE SYSTEM FORMA ASINCRONA       */
+/* --------------------------------------------------- */
+
+console.log("Inicio del programa");
+
+// se maneja con una callback que se pasan el error y los datos como parametros
+fs.readFile("./datosAsinc.txt", "utf-8", (error, datos) => {
+  if (error)
+    throw new Error(`Hubo un error en la lectura de archivo: ${error}`);
+  console.log("Async RD1 ok", datos, datos.length);
+
+  // escribo un archivo en forma asincronica que tambien devuelve un callback con el error
+  fs.writeFile("./datosAsinc.txt", new Date().toLocaleString(), (error) => {
+    if (error) throw new Error(`Error en escritura de archivo: ${error}`);
+    console.log("Async WR ok");
+
+    fs.readFile("./datosAsinc.txt", "utf-8", (error, datos) => {
+      if (error)
+        throw new Error(`Hubo un error en la lectura de archivo: ${error}`);
+      console.log("Async RD1 ok", datos, datos.length);
+    });
+  });
+});
+
+// al tener los procesos desorganizados y sin un orden nos encontramos con el problema de que escribio el archivo antes de leerlos
+// para poder solucionar esto vamos a poner que la escritura se realice dentro de la primera lectura y la segunda lectura dentro de la escritura
+
+// en caso de haber varios anidamientos se puede generar un "callback hell" que confunde.
+
+console.log("... otras tareas ...");
---
+++ Node.js/clase02/ejemploServerHttpFS/server.js
@@ -0,0 +1,72 @@
+const { fstat } = require("fs");
+const http = require("http");
+const fs = require('fs');
+
+let contadorVisitas = 0;
+
+
+const server = http.createServer((req, res) => {
+  let url = req.url;
+  let method = req.method;
+  
+  console.log(url, method);
+
+  if (method === "GET"){
+
+    
+    if (url === "/") {
+      res.writeHead(200, { "content-type": "text/html" }); // asume 'text/plain'
+      
+      res.write('<h2 style="color:purple">Hola soy un servidor HTTP</h2>');
+      
+      res.write(`<h3 style="color:crimson">Visitas: ${++contadorVisitas}</h3>`);
+      
+      res.write(`<p style="color:purple">La FyH actual es: ${new Date().toLocaleString()}</p>`);
+      
+      res.end()
+    }
+    else if (url ==='/page'){
+      let pathindex = './public/index.html';
+
+      /* ------------------------------------------ */
+      /*    Lectura Sincrónica (BLOQUEANTE) NO!!    */
+      /* ------------------------------------------ */
+
+      /* try {
+        // no se necesita aclarar utf-8 ya que se  puede trabajar con buffer array
+        let page = fs.readFileSync(pathindex)
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(page);
+      } catch (error) {
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.end(`<h2 style="color:red;">ERROR 404. Recurso <span style="color:black;">${pathindex}</span> no encontrado</h2>`)
+      } */
+
+      /* ------------------------------------------ */
+      /*    Lectura Sincrónica (BLOQUEANTE) NO!!    */
+      /* ------------------------------------------ */
+
+      // ! RETOMAR 1:32:00
+    }
+    else if (url ==="/reset"){
+      contadorVisitas = 0;
+      res.end('Reset OK')
+    }
+    else { 
+      res.writeHead(500, { "content-type": "text/html"});
+      
+      res.end(`<h2 style="color:red">ERROR 500. Ruta <span style="color:black;">${url}</span> no encontrado</h2>`);
+      
+    }
+  }
+  else {
+    res.writeHead(500, {'content-type': 'text/html'})
+    res.end(`<h2 style="color:red;">ERROR 500. Método <span style="color:black;">${method}</span> no implementado</h2>`)
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, (err) => {
+  if (err) return console.log(`Error en el servidor: ${err}`); //declaramos como manejar el error
+  console.log(`Servidor HTTP escuchando en el puerto ${PORT}`);
+});
